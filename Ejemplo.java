22839230fe646d15fd8ce71c90568810dfb2db8c
Adrián Barbos <adrianbarboss17@gmail.com>
2026-04-28 23:27:06 +0200

    Update Ejemplo.java

--- Ejemplo.java
+++ Ejemplo.java
@@ -2,7 +2,7 @@ public class Ejemplo {
 
     static void main(String[] args) {
 
-        String nombre = "Adrian";
+        String nombre = "Adrian Barbos";
         int numero = 19;
 
         System.out.println("Hola me llamo " + nombre + " y tengo " + numero + " años. " );
