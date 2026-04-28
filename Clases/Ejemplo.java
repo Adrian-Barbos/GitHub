21ed4c3d102f0cfd8171a18d2f8174e5368f265e
Adrian-Barbos <adrianbarboss17@gmail.com>
2026-04-28 23:00:28 +0200

    Hemos cambiado el int numero = 18, a int numero = 19;

--- Clases/Ejemplo.java
+++ Clases/Ejemplo.java
@@ -3,7 +3,8 @@ public class Ejemplo {
     static void main(String[] args) {
 
         String nombre = "Adrian";
-        int numero = 18;
+        int numero = 19;
+	
 
         System.out.println("Hola me llamo " + nombre + " y tengo " + numero + " años. " );
     }
